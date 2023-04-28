8b1ebe52f2dbb6b4671eb6efaacd3ba50bb00b3c
Hassan0Kamel <103977827+Hassan0Kamel@users.noreply.github.com>
2023-04-28 03:01:58 +0200

    Create mainx.c

---
+++ mainx.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+ * main - entint
+ * @acr: count
+ * @avr: vector
+ * Return: 0 or 1
+ */
+int main(int acr, char **avr)
+{
+	info_t info[] = { INFO_INIT };
+	int filedirectory = 2;
+
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (filedirectory)
+			: "r" (filedirectory));
+
+	if (acr == 2)
+	{
+		filedirectory = open(av[1], O_RDONLY);
+		if (filedirectory == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(avr[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(avr[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = filedirectory;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
