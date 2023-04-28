9c1ad5de4fd6f331ead21f75edad5c4fe3c25ba1
Hassan0Kamel <hassan.oka101@gmail.com>
2023-04-27 17:40:42 -0700

    noice

--- history.c
+++ history.c
@@ -11,7 +11,7 @@ char *get_history_file(info_t *infos)
 	char *buf, *directory;
 
 	directory = _getenv(infos, "HOME=");
-	if (!directroy)
+	if (!directory)
 		return (NULL);
 	buf = malloc(sizeof(char) * (_strlen(directory) + _strlen(HIST_FILE) + 2));
 	if (!buf)
@@ -31,15 +31,15 @@ char *get_history_file(info_t *infos)
 int write_history(info_t *infos)
 {
 	ssize_t filedirectory;
-	char *filename = get_history_file(info);
+	char *filename = get_history_file(infos);
 	list_t *nodeed = NULL;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	filedirectory = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
 	free(filename);
-	if (fd == -1)
+	if (filedirectory == -1)
 		return (-1);
 	for (nodeed = infos->history; nodeed; nodeed = nodeed->next)
 	{
@@ -61,23 +61,23 @@ int read_history(info_t *infos)
 	int i, last = 0, linecount = 0;
 	ssize_t filedirectory, rdlen, fsize = 0;
 	struct stat st;
-	char *buf = NULL, *filename = get_history_file(info);
+	char *buf = NULL, *filename = get_history_file(infos);
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	filedirectory = open(filename, O_RDONLY);
 	free(filename);
-	if (fd == -1)
+	if (filedirectory == -1)
 		return (0);
-	if (!fstat(fd, &st))
+	if (!fstat(filedirectory, &st))
 		fsize = st.st_size;
 	if (fsize < 2)
 		return (0);
 	buf = malloc(sizeof(char) * (fsize + 1));
 	if (!buf)
 		return (0);
-	rdlen = read(fd, buf, fsize);
+	rdlen = read(filedirectory, buf, fsize);
 	buf[fsize] = 0;
 	if (rdlen <= 0)
 		return (free(buf), 0);
@@ -95,7 +95,7 @@ int read_history(info_t *infos)
 	infos->histcount = linecount;
 	while (infos->histcount-- >= HIST_MAX)
 		delete_node_at_index(&(infos->history), 0);
-	renumber_history(info);
+	renumber_history(infos);
 	return (infos->histcount);
 }
 
@@ -112,7 +112,7 @@ int build_history_list(info_t *infos, char *buf, int linecounter)
 	list_t *nodeed = NULL;
 
 	if (infos->history)
-		nodeed = info->history;
+		nodeed = infos->history;
 	add_node_end(&nodeed, buf, linecounter);
 
 	if (!infos->history)
@@ -133,7 +133,7 @@ int renumber_history(info_t *infos)
 	while (nodeed)
 	{
 		nodeed->num = j++;
-		nodeed = node->next;
+		nodeed = nodeed->next;
 	}
 	return (infos->histcount = j);
 }
