0edf8ffed6b748df653b77c91986cb3ee2810a31
Hassan0Kamel <hassan.oka101@gmail.com>
2023-04-27 18:39:33 -0700

    lol

--- tokenizer.c
+++ tokenizer.c
@@ -12,12 +12,13 @@ char **strtow(char *string, char *ding)
 	int i, j, k, m, numwords = 0;
 	char **s;
 
-	if (str == NULL || string[0] == 0)
+	if (string == NULL || string[0] == 0)
 		return (NULL);
 	if (!ding)
 		ding = " ";
 	for (i = 0; string[i] != '\0'; i++)
-		if (!is_delim(string[i], d) && (is_delim(string[i + 1], d) || !string[i + 1]))
+		if (!is_delim(string[i], ding) &&
+				(is_delim(string[i + 1], ding) || !string[i + 1]))
 			numwords++;
 
 	if (numwords == 0)
@@ -27,10 +28,10 @@ char **strtow(char *string, char *ding)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (is_delim(string[i], d))
+		while (is_delim(string[i], ding))
 			i++;
 		k = 0;
-		while (!is_delim(string[i + k], d) && string[i + k])
+		while (!is_delim(string[i + k], ding) && string[i + k])
 			k++;
 		s[j] = malloc((k + 1) * sizeof(char));
 		if (!s[j])
@@ -41,7 +42,7 @@ char **strtow(char *string, char *ding)
 			return (NULL);
 		}
 		for (m = 0; m < k; m++)
-			s[j][m] = str[i++];
+			s[j][m] = string[i++];
 		s[j][m] = 0;
 	}
 	s[j] = NULL;
@@ -62,8 +63,8 @@ char **strtow2(char *string, char ding)
 	if (string == NULL || string[0] == 0)
 		return (NULL);
 	for (i = 0; string[i] != '\0'; i++)
-		if ((string[i] != d && string[i + 1] == ding) ||
-				    (string[i] != ding && !string[i + 1]) || string[i + 1] == d)
+		if ((string[i] != ding && string[i + 1] == ding) ||
+				    (string[i] != ding && !string[i + 1]) || string[i + 1] == ding)
 			numwords++;
 	if (numwords == 0)
 		return (NULL);
@@ -72,7 +73,7 @@ char **strtow2(char *string, char ding)
 		return (NULL);
 	for (i = 0, j = 0; j < numwords; j++)
 	{
-		while (string[i] == d && string[i] != ding)
+		while (string[i] == ding && string[i] != ding)
 			i++;
 		k = 0;
 		while (string[i + k] != ding && string[i + k] && string[i + k] != ding)
